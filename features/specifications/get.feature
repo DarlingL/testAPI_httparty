115956c68565d27a0ff0cab10d64a29df6120edb
darling.lopes <darling.lopes@concrete.com.br>
2017-10-04 15:58:18 -0300

    atualizado2

--- features/specifications/get.feature
+++ features/specifications/get.feature
@@ -5,5 +5,5 @@ Funcionalidade: Teste de API Verbo GET
 @executa_get
 Cenario: Automatizando GET
 
-  Dado que eu faça um GET no endpoint clientes
-  Entao o retorno dele será todos os clientes cadastrados
+  Dado que eu faca um GET no endpoint clientes
+  Entao o retorno dele sera todos os clientes cadastrados
--- features/step_definitions/get_step.rb
+++ features/step_definitions/get_step.rb
@@ -1,8 +1,9 @@
-Dado(/^que eu faça um GET no endpoint clientes$/) do
+
+Dado("que eu faca um GET no endpoint clientes") do
   @get_clientes = HTTParty.get 'http://localhost:3000/clientes'
 end
 
-Entao(/^o retorno dele será todos os clientes cadastrados$/) do
+Entao("o retorno dele sera todos os clientes cadastrados") do
   puts @get_clientes.body
   puts @get_clientes.code
   expect(@get_clientes.code).to eq 200
